f41818308fbb396a0cda99bfb2dfa7d7b317f7e6
Apoorv Rastogi <apoorv.r@HCL.COM>
2021-01-04 09:54:29 +0530

    Add Function

--- src/main/string_Programs_Apoorv/Duplicate_String_WithoutCollection_Apoorv.java
+++ src/main/string_Programs_Apoorv/Duplicate_String_WithoutCollection_Apoorv.java
@@ -3,25 +3,29 @@ package main.string_Programs_Apoorv;
 public class Duplicate_String_WithoutCollection_Apoorv {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
-     String Str= "eApooravp";
-     int count=0;
-     char[] dup=Str.toUpperCase().toCharArray();
-     for(int i=0;i<Str.length();i++)
-     {
-    	 for(int j=i+1;j<Str.length();j++)
-     
-     {
-    	if(dup[i] == dup[j])
-    	{
-     System.out.println(dup[j] + " ");
-        count++;
-        
-     }
-    	
-    	
-     }
-     }
-     System.out.println("Total Duplicate is" +" " + count);
+     duplicate_Count();
 }
+	public static int duplicate_Count() {
+		String Str= "eApooravp";
+	     int count=0;
+	     char[] dup=Str.toUpperCase().toCharArray();
+	     for(int i=0;i<Str.length();i++)
+	     {
+	    	 for(int j=i+1;j<Str.length();j++)
+	     
+	     {
+	    	if(dup[i] == dup[j])
+	    	{
+	     System.out.print(dup[j] + " ");
+	        count++;
+	        
+	    	}   	
+	     }
+	    }
+	     System.out.println(""); 
+	     System.out.println("Total Duplicate is" +" " + count);
+	     return count;
+	}
 }
