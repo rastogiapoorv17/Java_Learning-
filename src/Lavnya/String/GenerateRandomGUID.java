bb225a0807be4405df66e4abbb5fb05d73471189
lavnyakanti_upadhyay <lavnyaup@gmail.com>
2021-01-12 17:17:18 -0800

    String prg

---
+++ src/Lavnya/String/GenerateRandomGUID.java
@@ -0,0 +1,17 @@
+package Lavnya.String;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class GenerateRandomGUID {
+
+    public static void main(String[] args) {
+
+        String ss= (UUID.randomUUID()).toString();
+
+
+
+
+        System.out.println(ss);
+    }
+}
---
+++ src/Lavnya/String/TotalNumberOfgivenChar.java
@@ -0,0 +1,19 @@
+package Lavnya.String;
+
+public class TotalNumberOfgivenChar {
+
+    public static double getNumberOFGivenchar(String ss,char c)
+    {
+
+       //double i= ss.chars().filter().count();
+
+
+
+        return 0.0;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println((TotalNumberOfgivenChar.getNumberOFGivenchar("elloH",'H')));
+    }
+}
